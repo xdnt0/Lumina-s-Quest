5160d9f0fc51bae76988d515915d295b0462bdc2
Johan S. Ruiz <johanruiz060@gmail.com>
2025-07-13 13:22:03 -0500

    imagenes actualizadas

--- src/main/java/jo/student/proyectof/minijuegos/LaberintoView.java
+++ src/main/java/jo/student/proyectof/minijuegos/LaberintoView.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package jo.student.proyectof.minijuegos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
